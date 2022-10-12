5e4c30ec0b12729979fe194e588e23868678a76d
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2022-10-12 23:39:30 +0900

    Add files via upload

--- quake_test.py
+++ quake_test.py
@@ -21,41 +21,39 @@ class quake_mail_class():
         # password = "19960701jishin"
         password = "ubqrmyfjgxsojiwg"
 
+        # 送受信先
+        to_email = "jinchuanzhishu76@gmail.com"
+        from_email = "subaodezhen641@gmail.com"
 
-# 送受信先
-to_email = "jinchuanzhishu76@gmail.com"
-from_email = "subaodezhen641@gmail.com"
+        # MIMEの作成
+        subject = "地震速報"
+        # message = "地震速報"
+        message = quake_news
+        msg = MIMEText(message, "html")
+        msg["Subject"] = subject
+        msg["To"] = to_email
+        msg["From"] = from_email
 
-# MIMEの作成
-subject = "地震速報"
-# message = "地震速報"
-message = quake_news
-msg = MIMEText(message, "html")
-msg["Subject"] = subject
-msg["To"] = to_email
-msg["From"] = from_email
-
-# メール送信処理
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(account, password)
-server.send_message(msg)
-server.quit()
+        # メール送信処理
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(account, password)
+        server.send_message(msg)
+        server.quit()
 
 
 class y_news_class():
-def y_news(self, url):
-self.url = url
-data_bank = []
+    def y_news(self, url):
+        self.url = url
+        data_bank = []
 
-
-feed = feedparser.parse(url)
-for entry in feed.entries:
-    data_bank.append(entry.title)
-    data_bank.append(entry.link)
-    # print('タイトル:', entry.title)
-    # print('URL:', entry.link)
-return data_bank
+        feed = feedparser.parse(url)
+        for entry in feed.entries:
+            data_bank.append(entry.title)
+            data_bank.append(entry.link)
+            # print('タイトル:', entry.title)
+            # print('URL:', entry.link)
+        return data_bank
 
 url = "https://headline.5ch.net/bbynamazu/news.rss"
 
@@ -72,61 +70,55 @@ data_now = bbb
 path = './data/quake_data_test.txt'
 # path = '/home/natsukiogawa/sample/quake_data.txt'
 
+
 f = open(path, 'r', encoding='UTF-8')
 data_before = f.read()
 # print(data_before)
-
 
 # if (data_before == data_now):
 #     print("no change")
 # else:
 # print("There are some changes")
-###print("地震が発生しました[これはテストです]")
+print("地震が発生しました[これはテストです]")
 
 # path = './data/quake_data.txt'
 # path = './data/quake_data.txt'
-# path = './data/quake_data.txt'
 f = open(path, 'w')
-###f.write(data_now)  # 何も書き込まなくてファイルは作成されました
+f.write(data_now)  # 何も書き込まなくてファイルは作成されました
 data_now = list(data_now)
 
 
 ccc = y_news_class()
 ddd = aaa.y_news(url)
 # print(ddd)
-###res_date_before = res_list[0]
-
-# print(ddd[0])
 # print(type(ddd))
+# print(ddd[0])
 # print(type(ddd[0]))
 
-###message = ddd[0]
+message = ddd[0]
 
 
-
+res_list = re.findall(r'\d+', message)
 # print(res_list)
-###res_list = re.findall(r'\d+', message)
-###res_date = res_date_before[:4] + "年" + res_date_before[4:]
-###res_date = res_date[:7] + "月" + res_date[7:]
-###res_date = res_date[:10] + "日" + res_date[10:]
-###res_date = res_date[:13] + "時" + res_date[13:]
-###res_date = res_date[:16] + "分" + res_date[16:]
-###res_date = res_date[:19] + "秒" + res_date[19:]
+res_date_before = res_list[0]
+
+res_date = res_date_before[:4] + "年" + res_date_before[4:]
+res_date = res_date[:7] + "月" + res_date[7:]
+res_date = res_date[:10] + "日" + res_date[10:]
+res_date = res_date[:13] + "時" + res_date[13:]
+res_date = res_date[:16] + "分" + res_date[16:]
+res_date = res_date[:19] + "秒" + res_date[19:]
 # print(res_date)
 # res[7] = "年"
 # res[10] = "年"
 # massage = res
-###message = message.replace(res_date_before, res_date)
-message = "〇〇〇〇年〇〇月〇〇日〇〇時〇〇分〇〇秒　〇〇地域　M〇〇　深さ〇〇km　最大震度
-〇〇"
+message = message.replace(res_date_before, res_date)
+message = "〇〇〇〇年〇〇月〇〇日〇〇時〇〇分〇〇秒　〇〇地域　M〇〇　深さ〇〇km　最大震度〇〇"
 
 # time = datetime.datetime.now()   # 日付を取得する
 # time = time.strftime('%Y年%m月%d日 %H:%M:%S')   # 見やすく変換する
 
 # 以下、LINEのメッセージ送信プログラム
-
-
-####message = "[an earthquake happened]"
 
 TOKEN = 'ubh9DRuV9zdbxHuY7bRdyigA1nSVTaMj1O8E2oE9QJt'
 api_url = 'https://notify-api.line.me/api/notify'
@@ -137,7 +129,6 @@ send_contents = "地震が起きました : " + message + " "
 TOKEN_dic = {'Authorization': 'Bearer' + ' ' + TOKEN}
 send_dic = {'message': send_contents}
 
-print("地震が起きました")
 image_file = './test.png'
 binary = open(image_file, mode='rb')
 image_dic = {'imageFile': binary}
